eb23884f0ea98a19d704791fd7db02880bcbcfc0
Jim Poffenberger <jpoffy@ameritech.net>
2020-05-31 10:20:48 -0500

    Remove block read/write

--- src/main/java/memoryInterface/MemoryDriver.java
+++ src/main/java/memoryInterface/MemoryDriver.java
@@ -6,8 +6,7 @@ import exceptions.illegalAddressException;
 public interface MemoryDriver {
 	public byte read(int address) throws illegalAddressException, DeviceUnavailable;
 	public void write (int address, byte data) throws illegalAddressException, DeviceUnavailable ;
-	public void blockWrite(int startAddress, int length, byte[] data)  throws illegalAddressException;
-	public byte[] blockRead(int startAddress, int length) throws illegalAddressException;
+	
 	public void setIOPage(int address);
 	public int getIOPage();
 
