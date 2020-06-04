612eab29302455bda3d0a9ff179e7f7f04b6c6ec
Jim Poffenberger <jpoffy@ameritech.net>
2020-06-04 17:00:23 -0500

    internal instruction for intrupts

---
+++ src/main/java/Firmeware/Framework/MIH.java
@@ -0,0 +1,44 @@
+package Firmeware.Framework;
+
+import cpu001.CPU;
+import exceptions.DeviceUnavailable;
+import exceptions.illegalAddressException;
+
+public class MIH extends Instruction {
+
+	public MIH() {
+		super((byte) (0x3E));
+	}
+
+	public void exeute(CPU c) throws illegalAddressException, DeviceUnavailable {
+		// TODO Auto-generated method stub
+		int interuptHandlerAddress = 0; 
+		if (c.getNMInterruptFired()) {
+			interuptHandlerAddress = getNMInterruptHandlerAddress(c);
+		} else {
+			if (c.getInterruptFired()) {
+				interuptHandlerAddress = getInterruptHandlerAddress(c);
+			} else {
+				if (c.getPowerOnResetFired()) {
+					interuptHandlerAddress = getResetHandlerAddress(c);
+				}
+			}
+		}
+		if (interuptHandlerAddress != 0) {
+
+			int upper = (((c.pc) & 0xff00) >> 8);
+			int lower = (((c.pc) & 0x00ff));
+
+			// push on stack
+			c.sp--;
+			c.memory.write(c.sp, (byte) (upper & 0xff));
+			c.sp--;
+			c.memory.write(c.sp, (byte) (lower & 0xff));
+			c.sp--;
+			c.memory.write(c.sp, psr(c));
+
+			c.pc = interuptHandlerAddress;
+		}
+	}
+
+}
