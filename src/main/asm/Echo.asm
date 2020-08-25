4a07b0738e6ed3a66f9af841f994c39986e1be80
Jim Poffenberger <jpoffy@ameritech.net>
2020-08-25 16:20:03 -0500

    Updates to the Echo app

--- src/main/asm/Echo.asm
+++ src/main/asm/Echo.asm
@@ -1,15 +1,21 @@
 CPU 6502
 OUTPUT HEX
+PAGE 100,150
 
 * = $1000
 
 COUT =$EC00
 
-START   JSR PRT
-WAIT    LDA COUT
-        BEQ WAIT
+START   JSR PRT		;; print prompt
+WAIT    LDA COUT 	;; grab char from console - it though echos char.
+			;; currently it is setup to wait until a charactor
+			;; is typed but should read the status flag as to if
+			;; there is a character ready else return.
+        BEQ WAIT	;; if no charactor - then wait for one.
         STA COUT
-        JMP START
+        CMP	#$0a
+        BEQ START
+        JMP WAIT
         
 PRT     LDX #$10
         LDY #0
