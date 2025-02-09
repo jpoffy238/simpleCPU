c634bf3ddcb75bfa6e69eea8c7eb81e236d6c6a2
Jim Poffenberger <jpoffy@ameritech.net>
2025-02-09 17:40:27 -0600

    refactor code

--- src/main/java/com/mj/Registers/gpregister.java
+++ src/main/java/com/mj/Registers/gpregister.java
@@ -1,70 +1,73 @@
+
 package com.mj.Registers;
 
 import com.mj.exceptions.nflagException;
 import com.mj.exceptions.zflagException;
 
+// This is a 
 public class gpregister implements generalPurpose {
-	int value;
-	char name;
-	public int get() {
-		// TODO Auto-generated method stub
-		
-		return (value & 0xff);
-	}
+    int value;
+    char name;
 
-	public void set(int  value) throws zflagException, nflagException {
-		// TODO Auto-generated method stub
-		
-		this.value = value & 0xff;
-		if (0 == this.value ) {
-			throw new zflagException();
-		}
-		if ( (0x80 & this.value )!= 0 ) {
-			throw new nflagException();
-		}
-	
-	}
+    // Constructor to initialize the register's value and name
+    public gpregister(char name, int initialValue) {
+        this.name = name;
+        set(initialValue);
+    }
 
-	public void inc() throws zflagException, nflagException { 
-		// TODO Auto-generated method stub
-	value++;
-	value&=0x00ff;
-	
-	if (value == 0) {
-		throw new zflagException();
-	} 
-	
-	if ( (value&0x0080)  != 0 ) {
-		throw new nflagException();
-	}
-		
-	}
+    @Override
+    public int get() {
+        return (value & 0xff);
+    }
 
-	public void dec() throws nflagException, zflagException  {
-		
-		value--;
-		value&=0x00ff;
-		
-		if (value == 0) {
-			throw new zflagException();
-		} 
-		if ( (value&0x0080)  != 0 ) {
-			throw new nflagException();
-		}
-		
-		}
-	
+    @Override
+    public void set(int value) throws zflagException, nflagException {
+        this.value = value & 0xff;
+        if (this.value == 0) {
+            throw new zflagException();
+        }
+        if ((0x80 & this.value) != 0) {
+            throw new nflagException();
+        }
+    }
 
+    @Override
+    public void inc() throws zflagException, nflagException {
+        value++;
+        value &= 0xff;
 
-	public char name() {
-		// TODO Auto-generated method stub
-		return name;
-	}
+        if (value == 0) {
+            throw new zflagException();
+        } 
+        if ((value & 0x80) != 0) {
+            throw new nflagException();
+        }
+    }
 
-	public void reset() {
-		// TODO Auto-generated method stub
-		value = 0;
-		
-	}
+    @Override
+    public void dec() throws zflagException, nflagException {
+        value--;
+        value &= 0xff;
 
+        if (value == 0) {
+            throw new zflagException();
+        } 
+        if ((value & 0x80) != 0) {
+            throw new nflagException();
+        }
+    }
+
+    @Override
+    public char name() {
+        return name;
+    }
+
+    @Override
+    public void reset() {
+        value = 0;
+        // Resetting the name might not be necessary, but it's up to your design
+        // this.name = 'R'; // Example: resetting to a default name
+    }
 }
+
+
